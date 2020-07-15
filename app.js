3b66813005c0942f8481629d82d8ac6fd67d7ad0
bsteele97 <65141727+bsteele97@users.noreply.github.com>
2020-07-14 19:49:49 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables */
+const navbar = document.querySelector('#navbar__list');
+const allSections = document.querySelectorAll('section');
+
+
+
+ /* End Global Variables
+  Start Helper Functions */
+
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions */
+ object.onscroll = "section__container"(this.#home)
+ {myScript};
+
+ function section__container() {
+     document.getElementById("section__container").addEventListener
+     ("scroll", section__container);
+
+     function section__container() {
+
+        document.getElementById("home")innerHTML = "you scrolled on the landing page!"
+     }
+ }
+
+// build the nav
+const ul = document.querySelector("navbar__list");
+const section = document.querySelectorAll("section");
+
+const navbarbuilder = () => {
+    for (let i = 0; i < section.length; i++) {
+        const listelement= elementCreater("li");
+        const linkelement= elementCreater("a", {"href":
+    this.#section$,{i + 1};
+    linkelement.className ="menu__link";
+    linkelement.textContent = section;'${i+1}';
+    listelement.appendChild(linkelement);
+    ul.appendChild(listelement);
+
+     
+};
+
+//load navbar function 
+window.addEventListener("DOMContentLoaded", navbarbuilder);
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+function section__container = "scrollTO(#home)";
+
+/**
+ * End Main Functions
+ * Begin Events */
+document.addEventListner('click', function () { 
+    console.log('the page was clicked');
+})
+// Build menu 
+
+// Scroll to section on link click
+var scroll = document.getElementById("section__container").innerHTML
+
+function scrollToTop() {
+    scroll.scrollIntoView(true); //Top
+}
+
+function scrollToBottom() { 
+    scroll.scrollIntoView(false); //Bottom
+}
+
+// Set sections as active
+
+
