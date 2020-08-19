c054645e374c3fe53f1ff3aa9a8c5a4257de9a03
Brooke Steele <65141727+brookesteele@users.noreply.github.com>
2020-08-18 18:35:03 -0700

    Update app.js

--- JS Landing Page/app.js
+++ JS Landing Page/app.js
@@ -15,55 +15,26 @@
 
 /**
  * Define Global Variables */
-const navbar = document.querySelector('#navbar__list');
-const allSections = document.querySelectorAll('section');
-/* End Global Variables
+/* End Global Variables*/
 
-//populate the navbar
+ //populate the navbar
 const navbar = document.getElementById('#navbar__list');
 
 const allSections = document.querySelectorAll('section');
 
 function createNav(){
-for (let item of allSections){
+    for (let item of allSections){
+        let menuitem = document.createElement("li");
+        menuitem.className="menu__link";
+        menuitem.dataset.nav=item.id;
+        menuitem.innerText=item.dataset.nav;
+        navbarMenu.appendChild(menuitem);
+    }
 
-    let menuitem = document.createElement("li");
-
-    menuitem.className="menu__link";
-
-    menuitem.dataset.nav=item.id;
-
-    menuitem.innerText=item.dataset.nav;
-
-    navbarMenu.appendChild(menuitem);
-}
+    createNav()
  
  * Begin Main Functions */
 
-
-
-
-// build the nav
-var dynamicnav = []
-dynamicnav = ["home", "sectionone", "sectiontwo", "sectionthree", "sectionfour"];
-
-//nav container
-var container = document.getElementById("dynamicnav");
- 
-//function definition 
-function createDynamicNavbar(){
-}
-//create unordered list
-var navList = document.createElement("ul");
-navList.className ="unorderedlist";
-
-
-function mouseEnter() {
-    document.getElementById("#home").style.color = "pink";  
-}
-function mouseLeave() {
-    document.getElementById("#home").style.color = "black";
-}
   
         
 //listening for scrolling
@@ -74,45 +45,33 @@ function mouseLeave() {
 // Add class 'active' to section when near top of viewport
 
 
-// Scroll to anchor ID using scrollTO event
-//document.getElementById("home").addEventListener("scroll," ,myFunction)
-
-//function myFunction() {
-    //document.getElementById("section_container").innerHTML = "TAH DAH"
-//}
-
-//event listeners for each section 
-
 /**
  * End Main Functions
  * Begin Events */
-console.log('ahhhhhhhh')
 document.getElementById("firstbutton").addEventListener("click", function() {
     console.log('the first button was clicked')
-    document.getElementById('home').scrollIntoView()
+    document.getElementById('section1').scrollIntoView({behavior: "smooth"})
 })
 document.getElementById('secondbutton').addEventListener("click", function() {
-    document.getElementById('sectionone').scrollIntoView()
+    document.getElementById('section2').scrollIntoView({behavior: "smooth"})
 })
  document.getElementById('thirdbutton').addEventListener("click", function() {
-     document.getElementById('sectiontwo').scrollIntoView()
+     document.getElementById('section3').scrollIntoView({behavior: "smooth"})
  })
   document.getElementById('fourthbutton').addEventListener("click", function() {
-      document.getElementById('sectionthree').scrollIntoView()
+      document.getElementById('section4').scrollIntoView({behavior: "smooth"})
   })
-document.getElementById('fifthbutton').addEventListener("click", function() {
-    document.getElementById('sectionfour').scrollIntoView()
-})
+
 // Build menu 
 
 
 // Set sections as active*/
-const addActiveClass =(section__container) => {
+const addActiveClass =(navbar__menu) => {
     section__container.class.addActiveClass("active");
     
 }
 
 //remove active sections
-const removeActiveClass =(section__container) => {
+const removeActiveClass =(navbar__menu) => {
     section__container.removeActiveClass("active");
 }
