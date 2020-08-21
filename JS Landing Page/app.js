ab73ab4b2db8101fa10bc10d6ed7f3e31317a008
Brooke Steele <65141727+brookesteele@users.noreply.github.com>
2020-08-21 08:09:10 -0700

    Update app.js

--- JS Landing Page/app.js
+++ JS Landing Page/app.js
@@ -15,29 +15,31 @@
 
 /**
  * Define Global Variables */
-const box = document.querySelector("div.landing__container");
-const rect = landing__container.getBoundingClientRect();
-console.log(rect);
+const navbar = document.getElementById("navbar__list");
+const allSections = document.querySelectorAll('section');
+const unorderedList = document.getElementsByTagName("li");
 /* End Global Variables*/
 
-
- //populate the navbar
-const navbar = document.getElementById('navbar__list');
-
-const allSections = document.querySelectorAll('section');
-
+//populate the navbar
 function createNav(){
     for (let item of allSections){
         let menuitem = document.createElement("li");
+        let menuitem = document.createElement("a");
         menuitem.className="menu__link";
         menuitem.dataset.nav=item.id;
         menuitem.innerText=item.dataset.nav;
 
-        // addingclick even listenr on Nav link
+        // adding click even listener on Nav link
         menuitem.addEventListener('click', function () {
             item.scrollIntoView({behavior: "smooth"})
         })
         navbar.appendChild(menuitem);
+        //add ID
+        menuitem.id = sectionTitle;
+        menuitem.className = "navBar_list"
+        //add href
+        menuitem.appendChild(menuitem)
+        navbar.appendChild(menu);
     }
 
     createNav()
@@ -51,5 +53,24 @@ const removeActiveClass = (navbar__menu) => {
 };
 
 
+//event listeners 
+const clickPage = document.querySelector('body');
+clickPage.addEventListener('click', function () {
+
+});
+
 //view state as active
+let inView = function (section) {
+    let box = section.getBoundingClientRect();
+
+    return(
+        box.top >= 0 &&
+        box.left >= 0 &&
+        box.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        box.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+}
+
+//section events 
 
